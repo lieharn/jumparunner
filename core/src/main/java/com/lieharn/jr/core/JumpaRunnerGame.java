1a25b8d317ae18ff0bc9dea0d06c410c372f8beb
lieharn <mathias.robel@gameforge.com>
2013-07-10 10:46:06 +0200

    physic with wolrd gravity added

--- core/src/main/java/com/lieharn/jr/core/JumpaRunnerGame.java
+++ core/src/main/java/com/lieharn/jr/core/JumpaRunnerGame.java
@@ -1,6 +1,13 @@
 package com.lieharn.jr.core;
 
 import java.util.ArrayList;
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
 import playn.core.Canvas;
 import playn.core.CanvasImage;
 import playn.core.Game;
@@ -12,13 +19,15 @@ import playn.core.Pointer;
 
 public class JumpaRunnerGame extends Game.Default {
 
-    private float x = 24.0f;
-    private float y = 3.0f;
-    private CanvasImage bgImage;
-    private Image cloudImage;
-    private ImageLayer cloud;
+    CanvasImage bgImage;
+    ImageLayer bgLayer;
+    Image cloudImage;
+    ImageLayer cloudLayer;
+    float x;
+    float y;
     Image ballImage;
-    GroupLayer ballsLayer;
+    GroupLayer ballLayer;
+    World world;
 
     public JumpaRunnerGame() {
         super(25); // call update every 33ms (30 times per second)
@@ -28,77 +37,93 @@ public class JumpaRunnerGame extends Game.Default {
 
         public ImageLayer layer;
         float angle = 0.0f;
+        Body body;
 
         public Ball(Image image, float x, float y) {
+            float radius = 0.5f;
             layer = graphics().createImageLayer(image);
-            layer.setOrigin(ballImage.width() / 2f, ballImage.height() / 2f);
+            layer.setOrigin(image.width() / 2f, image.height() / 2f);
+            layer.setScale((radius * 2) / image.width(), (radius * 2) / image.height());
             layer.setTranslation(x, y);
+
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyType.DYNAMIC;
+            bodyDef.position = new Vec2(x, y);
+            body = world.createBody(bodyDef);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = new CircleShape();
+            fixtureDef.shape.m_radius = radius;
+            fixtureDef.density = 1.0f;
+            fixtureDef.friction = 0.3f;
+            body.createFixture(fixtureDef);
+            body.setTransform(new Vec2(x, y), angle);
         }
 
-        public void update(float delta) {
+        public void paint(float delta) {
+            layer.setTranslation(body.getPosition().x, body.getPosition().y);
+            angle += delta * 0.1f;
             layer.setRotation(angle);
-            angle += delta;
         }
     }
     ArrayList<Ball> balls = new ArrayList<Ball>();
+    float physUnitPerScreenUnit = 1 / 26.666667f;
+    int width = 640;
+    int height = 480;
 
     @Override
     public void init() {
-        // create and add background image layer
-//    Image bgImage = assets().getImage("images/bg.png");
-//    ImageLayer bgLayer = graphics().createImageLayer(bgImage);
-//    graphics().rootLayer().add(bgLayer);
-        int width = 640;
-        int height = 480;
         bgImage = graphics().createImage(width, height);
-
         Canvas canvas = bgImage.canvas();
         canvas.setFillColor(0xff87ceeb);
         canvas.fillRect(0, 0, width, height);
-
-        ImageLayer bg = graphics().createImageLayer(bgImage);
-        graphics().rootLayer().add(bg);
+        bgLayer = graphics().createImageLayer(bgImage);
+        graphics().rootLayer().add(bgLayer);
 
         cloudImage = assets().getImage("images/cloud.png");
-        cloud = graphics().createImageLayer(cloudImage);
-        graphics().rootLayer().add(cloud);
-
-        cloud.setTranslation(x, y);
-
-
+        cloudLayer = graphics().createImageLayer(cloudImage);
+        graphics().rootLayer().add(cloudLayer);
+        x = 24.0f;
+        y = 3.0f;
         ballImage = assets().getImage("images/ball.png");
-        ballsLayer = graphics().createGroupLayer();
-        graphics().rootLayer().add(ballsLayer);
+        ballLayer = graphics().createGroupLayer();
+        ballLayer.setScale(1f / physUnitPerScreenUnit);
+        graphics().rootLayer().add(ballLayer);
         pointer().setListener(new Pointer.Adapter() {
             @Override
             public void onPointerEnd(Pointer.Event event) {
-                Ball ball = new Ball(ballImage, event.x(), event.y());
+                Ball ball = new Ball(ballImage, physUnitPerScreenUnit * event.x(),
+                    physUnitPerScreenUnit * event.y());
                 balls.add(ball);
-                ballsLayer.add(ball.layer);
-
-//                ImageLayer ball = graphics().createImageLayer(ballImage);
-//                ball.setTranslation(event.x(), event.y());
-//                ballsLayer.add(ball);
+                ballLayer.add(ball.layer);
             }
         });
+        initPhysics();
+    }
+
+    public void initPhysics() {
+        Vec2 gravity = new Vec2(0.0f, 10.0f);
+        world = new World(gravity);
+        world.setWarmStarting(true);
+        world.setAutoClearForces(true);
     }
 
     @Override
     public void update(int delta) {
         // the background automatically paints itself, so no need to do anything here!
-
-        x += 0.1f * delta;
-        if (x > bgImage.width() + cloudImage.width()) {
-            x = -cloudImage.width();
-        }
-        cloud.setTranslation(x, y);
-
-        for (Ball ball : balls) {
-            ball.update(delta);
-        }
+        world.step(0.033f, 10, 10);
     }
 
     @Override
     public void paint(float delta) {
+
+        x += delta;
+        if (x > bgImage.width() + cloudImage.width()) {
+            x = -cloudImage.width();
+        }
+        cloudLayer.setTranslation(x, y);
+
+        for (Ball ball : balls) {
+            ball.paint(delta);
+        }
     }
 }
